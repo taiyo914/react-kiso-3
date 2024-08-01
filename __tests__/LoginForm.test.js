74a0a67f65f55cbc78e9fd299c30b70f8c1dbbf6
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-01 22:39:06 +0900

    できたー！station2もクリア

--- __tests__/LoginForm.test.js
+++ __tests__/LoginForm.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // for better assertions
+// import '@testing-library/jest-dom/extend-expect'; 
 import LoginForm from '../src/assets/LoginForm';
 
 test('LoginForm renders correctly', () => {
   render(<LoginForm />);
   
-  // 基本的な要素が存在することを確認します
   expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /Login/i })).toBeInTheDocument();
--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'tests',
   webServer: {
     command: 'npm run dev', 
     port: 5173,
